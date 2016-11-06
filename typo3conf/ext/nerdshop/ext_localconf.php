88de5eff61d7548a8872eab0aa4055f373780312
aschmutt <andrea@schmutt.de>
2016-11-06 14:13:35 +0100

    Extension Name changed

--- typo3conf/ext/nerdshop/ext_localconf.php
+++ typo3conf/ext/nerdshop/ext_localconf.php
@@ -5,7 +5,7 @@ if (!defined('TYPO3_MODE')) {
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
 	'Schmutt.' . $_EXTKEY,
-	'Shop',
+	'Nerdshop',
 	array(
 		'Product' => 'list, show, new, create, edit, update, delete, filter, addToCart',
 		'Order' => 'list, show, new, create, edit, update, delete, submit',
